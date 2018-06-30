0cb68801f937937b2ab309e3ea4b7efe8a13ad01
Abhisek Chandale <achandale@cricut.com>
2018-06-30 12:06:50 +0530

    changes 30/06

---
+++ app/src/main/java/com/oottru/internationalization/Util/Common.kt
@@ -0,0 +1,19 @@
+package com.oottru.internationalization.Util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
+class Common {
+
+    companion object {
+
+        fun isNetworkAvailable(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+            return if (connectivityManager is ConnectivityManager) {
+                val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+                networkInfo?.isConnected ?: false
+            } else false
+        }
+    }
+}
